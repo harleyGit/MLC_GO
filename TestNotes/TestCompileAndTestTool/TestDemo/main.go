cd2cc7b79c738a3070d89d2239ac4ff7268ca39e
GangHuang <harleysor@qq.com>
2025-01-19 20:09:27 +0800

    feat:编译和测试工具

---
+++ TestNotes/TestCompileAndTestTool/TestDemo/main.go
@@ -0,0 +1,18 @@
+/*
+ * @Author: GangHuang harleysor@qq.com
+ * @Date: 2025-01-17 14:15:58
+ * @LastEditors: GangHuang harleysor@qq.com
+ * @LastEditTime: 2025-01-17 14:36:56
+ * @FilePath: /MLC_GO/TestNotes/TestCompileAndTestTool/TestDemo/main.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hellow world!!!")
+}
+
