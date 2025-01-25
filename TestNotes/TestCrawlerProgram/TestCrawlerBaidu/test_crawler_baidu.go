47d9dd7856f5306be982867b074d8dfa24c18ad5
GangHuang <harleysor@qq.com>
2025-01-25 12:23:00 +0800

    feat:存储爬取文件

--- TestNotes/TestCrawlerProgram/TestCrawlerBaidu/test_crawler_baidu.go
+++ TestNotes/TestCrawlerProgram/TestCrawlerBaidu/test_crawler_baidu.go
@@ -2,7 +2,7 @@
  * @Author: GangHuang harleysor@qq.com
  * @Date: 2025-01-23 14:38:00
  * @LastEditors: GangHuang harleysor@qq.com
- * @LastEditTime: 2025-01-24 21:53:14
+ * @LastEditTime: 2025-01-25 11:13:02
  * @FilePath: /MLC_GO/TestNotes/TestCrawlerProgram/TestCrawlerBaidu/test_crawler_baidu.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE.
  */
@@ -10,13 +10,103 @@ package main
 
 import (
 	"fmt"
-
+	"os"
+	// "io"
+	"bufio"
 	"github.com/gocolly/colly"
 )
 
 func main() {
 	// 根据http://news.baidu.com开始抓取页面结果
 	//testCrawlerBaidu()
+
+	// 访问指定网址
+	//testCrawlerAppointWebpage("https://news.baidu.com")
+
+	// 存储爬虫文件
+	
+	testCrawlerAppointWebpageAndSaveTxt("https://news.baidu.com")
+}
+
+// 存储爬虫文件
+func testCrawlerAppointWebpageAndSaveTxt(urls string) {
+	// 创建 Collector 对象
+	c := colly.NewCollector()
+	/* 
+	* 是否抓取指定链接的网页内容
+	* 初始设置为不抓取指定链接的网页内容
+	 */
+	visited := false
+	// 使用 Collector 对象抓取 URL
+	c.OnResponse(func (r *colly.Response)  {
+		if !visited {
+			visited = true
+			r.Request.Visit("/get?q=2")
+		}
+	})
+
+	// 文件的创建和持续写入
+	filename := "news.txt"	// 文件名
+	var f *os.File
+	var fileErr error 
+	if testCheckFileExist(filename) {	// 如果文件存在
+		f, fileErr = os.OpenFile(filename, os.O_APPEND | os.O_WRONLY, 0666)	// 打开文件
+		if fileErr != nil {
+			fmt.Printf("❌ 打开文件 news.txt 失败：%v\n", fileErr)
+		}
+	} else {
+		f,_ = os.Create(filename)	// 创建文件
+		fmt.Println("🍎 新建文件 news.txt")
+
+	}
+	defer f.Close()	// 关闭文件
+	write := bufio.NewWriter(f)
+	defer write.Flush()
+
+	// 对指定链接网页内容进行处理
+	c.OnHTML("a[href]", func (e *colly.HTMLElement)  {
+		href := e.Text	// 获取指定链接的网页内容
+		write.WriteString("🍒 "+href+"\n")
+		fmt.Println("🍎 网页内容：",href)	// 打印指定链接的网页内容
+	})
+
+
+	c.Visit(urls)	//访问指定链接
+}
+/* 检测文件是否存在
+* fileName 文件名
+ */
+func testCheckFileExist(filename string) bool {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
+}
+
+/// 抓取指定连接的网页内容 urls: 指定链接
+func testCrawlerAppointWebpage(urls string) {
+	// 创建 Collector 对象
+	c := colly.NewCollector()
+	/* 
+	* 是否抓取指定链接的网页内容
+	* 初始设置为不抓取指定链接的网页内容
+	 */
+	visited := false
+	// 使用 Collector 对象抓取 URL
+	c.OnResponse(func (r *colly.Response)  {
+		if !visited {
+			visited = true
+			r.Request.Visit("/get?q=2")
+		}
+	})
+
+	// 对指定链接网页内容进行处理
+	c.OnHTML("a[href]", func (e *colly.HTMLElement)  {
+		href := e.Text	// 获取指定链接的网页内容
+		fmt.Println("🍎 网页内容：",href)	// 打印指定链接的网页内容
+	})
+	c.Visit(urls)	//访问指定链接
 }
 
 // / 爬虫百度新闻网页
