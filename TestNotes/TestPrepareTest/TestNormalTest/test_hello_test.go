e4d7e6d5a02dc8bd9f647b434a0e6659bb78121a
GangHuang <harleysor@qq.com>
2025-01-27 07:09:37 +0800

    feat:测试

---
+++ TestNotes/TestPrepareTest/TestNormalTest/test_hello_test.go
@@ -0,0 +1,14 @@
+/*
+ * @Author: GangHuang harleysor@qq.com
+ * @Date: 2025-01-26 14:13:10
+ * @LastEditors: GangHuang harleysor@qq.com
+ * @LastEditTime: 2025-01-26 14:17:45
+ * @FilePath: /MLC_GO/TestNotes/TestPrepareTest/TestNormalTest/test_hello_test.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+package testnormaltest
+
+func HelloTest() string {
+	return "🎾 Hello World"
+}
+
---
+++ TestNotes/TestPrepareTest/TestNormalTest/test_normal_test.go
@@ -0,0 +1,21 @@
+/*
+ * @Author: GangHuang harleysor@qq.com
+ * @Date: 2025-01-26 14:12:26
+ * @LastEditors: GangHuang harleysor@qq.com
+ * @LastEditTime: 2025-01-26 14:20:51
+ * @FilePath: /MLC_GO/TestNotes/TestPrepareTest/TestNormalTest/test_normal_test.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+package testnormaltest
+
+import "testing"
+
+func TestHello(t *testing.T){
+	result := HelloTest()
+	want := "Hello World"
+	if result == want {
+		t.Logf("🍭 Hello() = %v, want %v", result, want)
+	}else {
+		t.Errorf("🍭 Hello() = %v, want %v", result, want)
+	}
+}
