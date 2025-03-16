32d1d30d50384777de2d1e0cf096db5370dd1a2e
GangHuang <harleysor@qq.com>
2025-03-16 19:20:08 +0800

    docs: 代码解读

--- TestNotes/gRPC_practice/gRPC_practice_v2/client/stream_client/client.go
+++ TestNotes/gRPC_practice/gRPC_practice_v2/client/stream_client/client.go
@@ -2,7 +2,7 @@
  * @Author: GangHuang harleysor@qq.com
  * @Date: 2025-03-16 13:45:06
  * @LastEditors: GangHuang harleysor@qq.com
- * @LastEditTime: 2025-03-16 16:22:58
+ * @LastEditTime: 2025-03-16 19:19:49
  * @FilePath: /MLC_GO/TestNotes/gRPC_practice/gRPC_practice_v2/client/stream_client/client.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -27,7 +27,9 @@ func main() {
 }
 
 // gRPC流式客户端
+// 建立连接
 func gRPCStreamClient_test_v1() {
+	// 客户端使用 grpc.Dial 连接到服务器（9002 端口），并创建了 StreamServiceClient 对象
 	conn, err := grpc.Dial(":"+PORT, grpc.WithInsecure())
 	if err != nil {
 		logging.ErrInfo("流式客户端 grpc.Dial err: ", err)
@@ -42,24 +44,27 @@ func gRPCStreamClient_test_v1() {
 		logging.ErrInfo("流式客户端 printLists.err: ", err)
 	}
 
-	err = printRecord(client, &pb.StreamRequest{Pt: &pb.StreamPoint{Name: "gRPC Stream Client: Record", Value: 2018}})
+	err = printRecord(client, &pb.StreamRequest{Pt: &pb.StreamPoint{Name: "gRPC Stream Client: Record", Value: 2019}})
 	if err != nil {
 		logging.ErrInfo("流式客户端 printRecord.err: ", err)
 	}
 
-	err = printRoute(client, &pb.StreamRequest{Pt: &pb.StreamPoint{Name: "gRPC Stream Client: Route", Value: 2018}})
+	err = printRoute(client, &pb.StreamRequest{Pt: &pb.StreamPoint{Name: "gRPC Stream Client: Route", Value: 2020}})
 	if err != nil {
 		logging.ErrInfo("流式客户端 printRoute.err: ", err)
 	}
 }
 
+// 调用 List 方法（服务器端流)
 func printLists(client pb.StreamServiceClient, r *pb.StreamRequest) error {
+	// 客户端调用 List 方法，获取一个流对象。
 	stream, err := client.List(context.Background(), r)
 	if err != nil {
 		return err
 	}
 
 	for {
+		// 在循环中不断调用 stream.Recv() 获取服务器端发送的响应，直到遇到 io.EOF 表示数据全部接收完毕。
 		resp, err := stream.Recv()
 		if err == io.EOF {
 			break
@@ -68,37 +73,45 @@ func printLists(client pb.StreamServiceClient, r *pb.StreamRequest) error {
 			return err
 		}
 
-		logging.DebugInfo("gRPC 流式客户端-printLists resp: pj.name: ", resp.Pt.Name, "pt.value: ", resp.Pt.Value)
+		logging.DebugInfo("gRPC 流式客户端方法printLists resp- pj.name: ", resp.Pt.Name, "🍔 pt.value: ", resp.Pt.Value)
 	}
 
 	return nil
 	
 }
 
+// 调用 Record 方法（客户端流）
 func printRecord(client pb.StreamServiceClient, r *pb.StreamRequest) error {
+	// 客户端调用 Record 方法后，获取一个流对象。
 	stream, err := client.Record(context.Background())
 	if err != nil {
 		return err
 	}
 
 	for n := 0; n < 6; n++ {
+		// 客户端连续调用 stream.Send() 发送多个请求消息。
 		err := stream.Send(r)
 		if err != nil {
 			return err
 		}
 	}
 
+	// 当数据发送完毕后，调用 stream.CloseAndRecv() 关闭发送通道并等待服务器的响应。
 	resp, err := stream.CloseAndRecv()
 	if err != nil {
 		return err
 	}
 
-	logging.DebugInfo("gRPC 流式客户端-printRecord resp: pj.name: ", resp.Pt.Name, "pt.value: ", resp.Pt.Value)
+	logging.DebugInfo("gRPC 流式客户端方法printRecord resp- pj.name: ", resp.Pt.Name, "🍭 pt.value: ", resp.Pt.Value)
 
 	return nil
 }
 
 // 双向流式-路由
+// 调用 Route 方法（双向流）
+// 对于双向流，客户端既发送消息，也接收服务器的消息。
+// 在每个循环中，客户端发送一条消息，然后接收一条响应。
+// 最后调用 stream.CloseSend() 关闭发送通道。
 func printRoute(client pb.StreamServiceClient, r *pb.StreamRequest) error {
 	stream, err := client.Route(context.Background())
 	if err != nil {
@@ -119,7 +132,7 @@ func printRoute(client pb.StreamServiceClient, r *pb.StreamRequest) error {
 			return err
 		}
 
-		logging.DebugInfo("gRPC 流式客户端-printRoute resp: pj.name: ", resp.Pt.Name, "pt.value: ", resp.Pt.Value)
+		logging.DebugInfo("gRPC 流式客户端-printRoute resp- pj.name: ", resp.Pt.Name, "🥑 pt.value: ", resp.Pt.Value)
 	}
 
 	stream.CloseSend()
--- TestNotes/gRPC_practice/gRPC_practice_v2/server/stream_server/server.go
+++ TestNotes/gRPC_practice/gRPC_practice_v2/server/stream_server/server.go
@@ -2,7 +2,7 @@
  * @Author: GangHuang harleysor@qq.com
  * @Date: 2025-03-16 13:44:39
  * @LastEditors: GangHuang harleysor@qq.com
- * @LastEditTime: 2025-03-16 16:19:25
+ * @LastEditTime: 2025-03-16 17:12:17
  * @FilePath: /MLC_GO/TestNotes/gRPC_practice/gRPC_practice_v2/server/stream_server/server.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -22,6 +22,9 @@ type StreamService struct{
 	// 新版 protoc-gen-go-grpc 生成的 gRPC 代码中，所有 service 定义的接口都会包含一个默认的未实现结构体:
 	// 		type UnimplementedStreamServiceServer struct{}
 	// 添加一个空实现
+	// 嵌入默认的未实现结构体，确保实现接口要求
+	// 这里 StreamService 结构体嵌入了 pb.UnimplementedStreamServiceServer，这样可以避免因为未实现所有接口方法而出错。
+	// 这也是新版 protoc-gen-go-grpc 的要求。
 	pb.UnimplementedStreamServiceServer
 }
 
@@ -35,16 +38,22 @@ func main() {
 
 // 流式gRPC的服务端
 func gRPCStreamServerPractice_v1() {
+	// 创建 gRPC 服务器，并注册 StreamService 服务。
 	server := grpc.NewServer()
 	pb.RegisterStreamServiceServer(server, &StreamService{})
-
+	
+	// 在指定端口（9002）监听，然后调用 server.Serve(lis) 启动服务。
 	lis, err := net.Listen("tcp", ":"+PORT)
 	if err != nil {
-		logging.ErrInfo("net.Listen err: %v", err)
+		logging.ErrInfo("net.Listen err: ", err)
 	}
 
 	server.Serve(lis)
 }
+
+// List 方法（服务器端流式 RPC）
+// 客户端调用 List 后，服务器端会在一个循环中调用 stream.Send()，连续发送多个响应消息。
+// 每个响应消息中，Value 字段的值逐渐增加。
 func (s *StreamService) List(r *pb.StreamRequest, stream pb.StreamService_ListServer) error {
 	for n := 0; n <= 6; n++ {
 		err := stream.Send(&pb.StreamResponse{
@@ -61,6 +70,9 @@ func (s *StreamService) List(r *pb.StreamRequest, stream pb.StreamService_ListSe
 	return nil
 }
 
+// Record 方法（服务端流式 RPC）
+// 在 Record 方法中，服务器端不断调用 stream.Recv() 来接收客户端发送的数据。
+// 当客户端发送完数据并关闭流后（返回 io.EOF），服务器调用 SendAndClose 发送最终响应，然后结束方法。
 func (s *StreamService) Record(stream pb.StreamService_RecordServer) error {
 	for {
 		r, err := stream.Recv()
@@ -72,13 +84,16 @@ func (s *StreamService) Record(stream pb.StreamService_RecordServer) error {
 			return err
 		}
 
-		logging.ErrInfo("gRPC 流式服务端-Record stream.Recv pt.name: ", r.Pt.Name,  "pt.value: ", r.Pt.Value)
+		logging.DebugInfo("gRPC 流式服务端-Record stream.Recv pt.name: ", r.Pt.Name,  "🥕 pt.value: ", r.Pt.Value)
 	}
 
 	return nil
 }
 
-// 双向流式 RPC- 路由
+// Route 方法（双向流式 RPC
+// 双向流中，客户端和服务器可以互相发送和接收消息。
+// 服务器端在一个循环中：先调用 stream.Send() 发送一条消息，然后调用 stream.Recv() 等待客户端的消息。
+// 当接收到 io.EOF 时，表示客户端已经关闭发送通道，服务器返回结束。
 func (s *StreamService) Route(stream pb.StreamService_RouteServer) error {
 	n := 0
 	for {
@@ -102,7 +117,7 @@ func (s *StreamService) Route(stream pb.StreamService_RouteServer) error {
 
 		n++
 
-		logging.DebugInfo("gRPC 流式服务端 路由-Route stream.Recv pt.name: ", r.Pt.Name, "pt.value: ",  r.Pt.Value)
+		logging.DebugInfo("gRPC 流式服务端 路由-Route stream.Recv pt.name: ", r.Pt.Name, "🍏 pt.value: ",  r.Pt.Value)
 	}
 
 	return nil
