a06c41d2517d98b3b54c29f156397881e768c1db
GangHuang <harleysor@qq.com>
2025-03-21 10:42:14 +0800

    feat: yml文件配置读取

--- TestNotes/unfamiliar_grammar_practice/read_file_practice/read_file_practice_main.go
+++ TestNotes/unfamiliar_grammar_practice/read_file_practice/read_file_practice_main.go
@@ -2,7 +2,7 @@
  * @Author: GangHuang harleysor@qq.com
  * @Date: 2025-03-20 21:07:44
  * @LastEditors: GangHuang harleysor@qq.com
- * @LastEditTime: 2025-03-21 09:51:14
+ * @LastEditTime: 2025-03-21 10:34:39
  * @FilePath: /MLC_GO/TestNotes/unfamiliar_grammar_practice/read_config_file_practice/read_config_json_practice.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,10 +11,16 @@ package read_file_practice
 import (
 	"MLC_GO/TestNotes/unfamiliar_grammar_practice/read_file_practice/read_json_file_practice_v"
 	"MLC_GO/TestNotes/unfamiliar_grammar_practice/read_file_practice/read_txt_file_practice_v"
+	"MLC_GO/TestNotes/unfamiliar_grammar_practice/read_file_practice/read_yml_file_practice_v"
 )
 
 
 func ReadFilePracticeMain() {
+	readYMLFile := read_yml_file_practice_v.ReadYMLTFilePractice{}
+	readYMLFile.ExecutePracticeNone()
+	readYMLFile.ReadYMLFilePractice_v1()
+
+	return
 
 	readTxtFile := read_txt_file_practice_v.ReadTXTFilePractice{}
 	readTxtFile.ExecutePracticeNone()
---
+++ TestNotes/unfamiliar_grammar_practice/read_file_practice/read_yml_file_practice_v/read_yml_file_practice_v1.go
@@ -0,0 +1,75 @@
+/*
+ * @Author: GangHuang harleysor@qq.com
+ * @Date: 2025-03-21 09:55:10
+ * @LastEditors: GangHuang harleysor@qq.com
+ * @LastEditTime: 2025-03-21 10:37:02
+ * @FilePath: /MLC_GO/TestNotes/unfamiliar_grammar_practice/read_file_practice/read_yaml_file_practice_v/read_yml_file_practice_v1.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+package read_yml_file_practice_v
+
+import (
+	"MLC_GO/pkg/hglog"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+//解析yml文件
+type BaseInfo struct {
+	Port     string `yaml:"port"`
+	Ip     	 string `yaml:"ip"`
+	Host     string `yaml:"host"`
+	Spring 	 RedisEntity `yaml:"spring"`
+}
+
+type RedisEntity struct {
+	Redis     RedisData `yaml:"redis"`
+}
+
+type RedisData struct {
+	Host     	string `yaml:"host"`
+	Port     	string `yaml:"port"`
+	DataBase    string `yaml:"dataBase"`
+	Timeout     string `yaml:"timeout"`
+}
+
+type ReadYMLTFilePractice struct {}
+
+// 协议
+func (readYMLPractice *ReadYMLTFilePractice) ExecutePracticeNone() {
+	hglog.DebugInfo("协议 读取yml文件配置 ReadJSONFilePractice ExecutePracticeNone")
+}
+
+func (readJSONPractice *ReadYMLTFilePractice) ReadYMLFilePractice_v1() {
+	baseInfo  := BaseInfo{}
+	// config := baseInfo.getConf("./conf/mlc_app.yml")
+	// hglog.DebugInfo("yml 文件读取内容: ip=",string(config.Host)," port=",string(config.Port))
+
+	baseInfo.getConf_v1("./conf/mlc_app.yml")
+	hglog.DebugInfo("yml 文件读取内容: ip=",string(baseInfo.Host)," port=",string(baseInfo.Port))
+
+}
+
+func (c *BaseInfo) getConf(path string) *BaseInfo {
+	yamlFile, err := os.ReadFile(path)
+	if err != nil {
+		hglog.ErrInfo("yml读取文件失败: ", err.Error())
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		hglog.ErrInfo("yml赋值失败: ",err.Error())
+	}
+	return c
+}
+
+func (c *BaseInfo) getConf_v1(path string) {
+	yamlFile, err := os.ReadFile(path)
+	if err != nil {
+		hglog.ErrInfo("yml读取文件失败: ", err.Error())
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		hglog.ErrInfo("yml赋值失败: ",err.Error())
+	}
+}
