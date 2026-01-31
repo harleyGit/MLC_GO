05d900e678abaceb324eb2b35bba04232b0571a2
GangHuang <harleysor@qq.com>
2026-01-31 21:54:21 +0800

    feat: 🍒  发送到前端空字段去除

--- internal/modules/user/dto/hg_user_dto.go
+++ internal/modules/user/dto/hg_user_dto.go
@@ -2,7 +2,7 @@
 * @Author: GangHuang harleysor@qq.com
 * @Date: 2026-01-21 16:00:24
  * @LastEditors: GangHuang harleysor@qq.com
- * @LastEditTime: 2026-01-28 20:52:50
+ * @LastEditTime: 2026-01-31 21:47:57
 
 * @FilePath: /MLC_GO/internal/modules/user/dto/hg_user_dto.go
 * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
@@ -16,17 +16,17 @@ import HGResponsePakcage "MLC_GO/internal/response"
 
 // 创建用户（POST）
 // TODO: 定一个基类结构体，有 Code和message，然dto有这2个变量，方便在协议返回。否则这样写死了
-type HGCreateUserDTO struct {
-	ID           int64
-	UserID       *string `json:"userID"`
-	Username     *string `json:"userName"`
-	Email        *string `json:"emial"`
-	Phone        *string `json:"phone"`
-	Code         *string `json:"code"`
-	Passowrd     string  `json:"password"`
-	PasswordHash *string `json:"passwordHash"`
-	Salt         *string `json:"salt"`
-	Token        *string `json:"token"`
+type HGCreateUserDTO struct { // 让 JSON 响应“只包含有意义的数据”，提升 API 的简洁性、安全性、可维护性和用户体验。
+	ID           int64   `json:"id,omitempty"`
+	UserID       *string `json:"userID,omitempty"`
+	Username     *string `json:"userName,omitempty"`
+	Email        *string `json:"email,omitempty"`
+	Phone        *string `json:"phone,omitempty"`
+	Code         *string `json:"code,omitempty"`
+	Password     string  `json:"password,omitempty"`
+	PasswordHash *string `json:"passwordHash,omitempty"`
+	Salt         *string `json:"salt,omitempty"`
+	Token        *string `json:"token,omitempty"`
 }
 
 /* 实现协议可选 */
